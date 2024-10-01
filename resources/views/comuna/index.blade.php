ba0870aa4942080867678ce7b365e8f04df6a7a8
Alejandro <mariaeugenialopez456@gmail.com>
2024-09-30 20:16:04 -0500

    First Commit

---
+++ resources/views/comuna/index.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Code</th>
+      <th scope="col">Comune</th>
+      <th scope="col">Municipality</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+       @foreach ($comunas as $comuna)
+    <tr>
+      <th scope="row">{{ $comuna->comu_codi }}</th>
+      <td>Mar{{ $comuna->comu_nomb }}k</td>
+      <td>{{ $comuna->muni_codi}}</td>
+      <td>Actions</td>
+    </tr>
+       @endforeach
+  </tbody>
+</table>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/comunas', [ComunaController::class,'index']);
+
+
