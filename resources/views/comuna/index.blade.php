e721cac9589bc3554ee65a060401e9740f75f52e
Alejandro <mariaeugenialopez456@gmail.com>
2024-10-01 19:38:39 -0500

    Other commi

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -8,9 +8,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Listado de Communas</title>
+    
   </head>
   <body>
+  <h1>Comunas</h1>
+    <a href="{{route('comunas.create')}}" class="btn btn-success">Add</a>
   <table class="table">
   <thead>
     <tr>
